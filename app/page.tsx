4a23ca5bf390e13c1462c90fce7508a5db7393b2
Sihle Gqeza <iamsihlegqeza@gmail.com>
2025-06-13 14:58:22 +0200

    Building home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import Container from '@/components/Container'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div className='p-10 bg-shop_light_pink'>
+    <Container className='p-10 bg-shop_light_pink'>
       <h2 className='text-xl font-semibold'>Home page</h2>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus aliquam repudiandae quisquam ut quaerat, fugiat, laborum quam hic explicabo facere repellendus fugit in maxime sit facilis architecto quo beatae quod pariatur quia unde aut. Id, placeat iure beatae illo voluptates labore aliquam, saepe iste, reiciendis obcaecati veniam dolorum nulla molestiae.</p>
-    </div>
+    </Container>
   )
 }
 
