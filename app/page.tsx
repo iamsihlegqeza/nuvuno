82b2bdfc03eafcbd564f9f8667e2afd854cfb406
Sihle Gqeza <iamsihlegqeza@gmail.com>
2025-06-13 09:47:28 +0200

    adding accordion badge card carousel checkbo x collapsible command dialog input label popover radio- group scroll-area separator table textarea tooltip

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <h2>Home page</h2>
+    <div className='p-10 bg-shop_light_pink'>
+      <h2 className='text-xl font-semibold'>Home page</h2>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus aliquam repudiandae quisquam ut quaerat, fugiat, laborum quam hic explicabo facere repellendus fugit in maxime sit facilis architecto quo beatae quod pariatur quia unde aut. Id, placeat iure beatae illo voluptates labore aliquam, saepe iste, reiciendis obcaecati veniam dolorum nulla molestiae.</p>
     </div>
   )
