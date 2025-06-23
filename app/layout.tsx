21ea46347ee0c94ec916d232354e5b6668c4c212
Sihle Gqeza <iamsihlegqeza@gmail.com>
2025-06-23 11:37:11 +0200

    schema types and sanity studio created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,21 @@
-import type { Metadata } from "next";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { ClerkProvider } from "@clerk/nextjs";
-
-export const metadata: Metadata = {
-  title: {
-    template: "%s - NuVuno online store",
-    default: "NuVuno online store",
-  },
-  description: "NuVuno online store, Your one stop shop for all your needs",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+import "./globals.css";
+import { Toaster } from "react-hot-toast";
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-1">{children}</main>
-        <Footer />
-      </div>
-    </ClerkProvider>
+    <html lang="en">
+      <body className="font-poppins antialiased">
+        {children}
+        <Toaster
+          position="bottom-right"
+          toastOptions={{
+            style: {
+              background: "#000000",
+              color: "#fff",
+            },
+          }}
+        />
+      </body>
+    </html>
   );
-}
+};
+export default RootLayout;
